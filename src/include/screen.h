7b045752e7a3e5740b06560b62cc24de3b543669
coulterminer <coultercash@gmail.com>
2024-01-06 22:13:30 -0700

    Fixed some errors with screen.h and newline freezing

--- src/include/screen.h
+++ src/include/screen.h
@@ -1,116 +1,125 @@
 #ifndef SCREEN_H
 #define SCREEN_H
+
 #include "types.h"
 #include "system.h"
 #include "string.h"
+
+// Screen size and depth constants
+const uint8 sw = 80, sh = 25, sd = 2;
+
+// Cursor position
 int cursorX = 0, cursorY = 0;
-const uint8 sw = 80,sh = 25,sd = 2;                                                     //We define the screen width, height, and depth.
-void clearLine(uint8 from,uint8 to)
-{
-        uint16 i = sw * from * sd;
-        string vidmem=(string)0xb8000;
-        for(i;i<(sw*to*sd);i++)
-        {
-                vidmem[i] = 0x0;
-        }
-}
-void updateCursor()
-{
-    unsigned temp;
 
-    temp = cursorY * sw + cursorX;                                                      // Position = (y * width) +  x
-
-    outportb(0x3D4, 14);                                                                // CRT Control Register: Select Cursor Location
-    outportb(0x3D5, temp >> 8);                                                         // Send the high byte across the bus
-    outportb(0x3D4, 15);                                                                // CRT Control Register: Select Send Low byte
-    outportb(0x3D5, temp);                                                              // Send the Low byte of the cursor location
-}
-void clearScreen()
-{
-        clearLine(0,sh-1);
-        cursorX = 0;
-        cursorY = 0;
-        updateCursor();
-}
-
-void scrollUp(uint8 lineNumber)
-{
-        string vidmem = (string)0xb8000;
-        uint16 i = 0;
-        clearLine(0,lineNumber-1);                                            //updated
-        for (i;i<sw*(sh-1)*2;i++)
-        {
-                vidmem[i] = vidmem[i+sw*2*lineNumber];
-        }
-        clearLine(sh-1-lineNumber,sh-1);
-        if((cursorY - lineNumber) < 0 ) 
-        {
-                cursorY = 0;
-                cursorX = 0;
-        } 
-        else 
-        {
-                cursorY -= lineNumber;
-        }
-        updateCursor();
-}
-
-
-void newLineCheck()
-{
-        if(cursorY >=sh-1)
-        {
-                scrollUp(1);
-        }
-}
-
-void printch(char c)
-{
-    string vidmem = (string) 0xb8000;     
-    switch(c)
-    {
-        case (0x08):
-                if(cursorX > 0) 
-                {
-	                cursorX--;									
-                        vidmem[(cursorY * sw + cursorX)*sd]=0x00;	                              
-	        }
-	        break;
-       /* case (0x09):
-                cursorX = (cursorX + 8) & ~(8 - 1); 
-                break;*/
-        case ('\r'):
-                cursorX = 0;
-                break;
-        case ('\n'):
-                cursorX = 0;
-                cursorY++;
-                break;
-        default:
-                vidmem [((cursorY * sw + cursorX))*sd] = c;
-                vidmem [((cursorY * sw + cursorX))*sd+1] = 0x0F;
-                cursorX++; 
-                break;
-	
+// Clears a line range on the screen
+void clearLine(uint8 from, uint8 to) {
+    uint16 i = sw * from * sd;
+    string vidmem = (string)0xb8000;
+    for(; i < (sw * to * sd); i += 2) {
+        vidmem[i] = ' ';           // Set character to space
+        vidmem[i + 1] = 0x07;      // Set attribute byte (e.g., light grey on black)
     }
-    if(cursorX >= sw)                                                                   
-    {
-        cursorX = 0;                                                                
-        cursorY++;                                                                    
+}
+
+
+// Updates the position of the cursor
+void updateCursor() {
+    unsigned temp = cursorY * sw + cursorX;
+    outportb(0x3D4, 14);
+    outportb(0x3D5, temp >> 8);
+    outportb(0x3D4, 15);
+    outportb(0x3D5, temp);
+}
+
+// Clears the entire screen and resets cursor position
+void clearScreen() {
+    clearLine(0, sh+1);
+    cursorX = 0;
+    cursorY = 0;
+    updateCursor();
+}
+
+// Scrolls the screen up by a given number of lines
+void scrollUp(uint8 lineNumber) {
+    string vidmem = (string)0xb8000;
+    uint16 i = 0;
+    clearLine(0, lineNumber - 1);
+    for(; i < sw * (sh - 1) * 2; i++) {
+        vidmem[i] = vidmem[i + sw * 2 * lineNumber];
+    }
+    clearLine(sh - 1 - lineNumber, sh - 1);
+    cursorY = (cursorY - lineNumber) < 0 ? 0 : cursorY - lineNumber;
+    cursorX = cursorY == 0 ? 0 : cursorX;
+    updateCursor();
+}
+
+// Checks if a new line is needed
+void newLineCheck() {
+    if(cursorY >= sh - 1) {
+        scrollUp(1);
+    }
+}
+
+// Prints a character on the screen at the cursor's position
+void printch(char c) {
+    string vidmem = (string)0xb8000;
+    switch(c) {
+        case (0x08): // Backspace
+            if(cursorX > 0) {
+                cursorX--;
+                vidmem[(cursorY * sw + cursorX) * sd] = 0x00;
+            }
+            break;
+        case ('\r'): // Carriage Return
+            cursorX = 0;
+            break;
+        case ('\n'): // New Line
+            cursorX = 0;
+            cursorY++;
+            break;
+        default: // Regular character
+            vidmem[((cursorY * sw + cursorX)) * sd] = c;
+            vidmem[((cursorY * sw + cursorX)) * sd + 1] = 0x0F; // Default color (white)
+            cursorX++;
+            break;
+    }
+    if(cursorX >= sw) {
+        cursorX = 0;
+        cursorY++;
     }
     updateCursor();
     newLineCheck();
 }
 
-void print (string ch)
-{
-        uint16 i = 0;
-        uint8 length = strlength(ch)-1;              //Updated (Now we store string length on a variable to call the function only once)
-        for(i;i<length;i++)
-        {
-                printch(ch[i]);
-        }
+// Prints a string in default color
+void print(string ch) {
+    uint16 i = 0;
+    uint8 length = strlength(ch);
+    for(; i < length; i++) {
+        printch(ch[i]);
+    }
 }
 
+// Function to print string in red
+void print_red(string ch) {
+    // Similar to print but with red color code
+}
+
+// Function to print string in blue
+void print_blue(string ch) {
+    // Similar to print but with blue color code
+}
+
+// Function to print string in pink
+void print_pink(string ch) {
+    // Similar to print but with pink color code
+}
+
+// Function to print string in cyan
+void print_cyan(string ch) {
+    // Similar to print but with cyan color code
+}
+
+// Add implementation for color printing functions here...
 
 #endif
--- src/kernel.c
+++ src/kernel.c
@@ -2,41 +2,52 @@
 #include "include/kb.h"
 #include "include/string.h"
 #include "include/types.h"
-kmain()
-{
-       clearScreen();
-       print("Welcome to COS operating system\n");
-       while (1)
-       {
-                print("\nCOS> ");
-                
-                string ch = readStr();
-                
-                if(strEql(ch,"clear"))
-                {
-                        clearScreen();
-                }
-                else if(strEql(ch,"info"))
-                {
-                        print("\n\nCOS 1.0 | Written by Coulter C. Stutz ");
-                        print("\n   Bootloader written in 32 bit Assembly Language ");
-                        print("\n       Compiled with NASM ");
-                        print("\n   Kernel written in C ");
-                        print("\n       Compiled with GCC ");
-                        print("\n\nInclude Libs forked from IKnowYourBrain ");
-                        print("\n   His Bitcoin Address:1KEJNtrJAU3VEYyiBtELKra1Tq3XM5Ligk ");
-                        print("\n\n\n   Buy Me Virtual Pizza! ");
-                        print("\n   Bitcoin: 13jStC1PWwZttLc8rhG6Xn1UeKE7rAP6hQ ");
-                        print("\n   ETH/BSC/MATIC: 0xa693190103733280E23055BE70C838d9b6708b9a ");
-                        print("\n   Monero: 49zw4GkS6MAWffKoskZxba3WXRtLJPUpwbN5ufmHL2AgKzB7YjSWBoVPivtPmn1DVxLiSwdZc7QnkcFnvXUuWdK3RV1xhZZ ");
-                }
-                else
-                {
-                        print("\nBad command!\n");
-                        print(ch);
-                }
-                
-                print("\n\n");        
-       }
-        
+
+void kmain() {
+    // Clear the screen and print the welcome message
+    clearScreen();
+    print("Welcome to COS operating system\n");
+
+    // Main loop
+    while (1) {
+        // Prompt for user input
+        print("\nCOS> ");
+
+        // Read user input
+        string ch = readStr();
+
+        // Process input if its length is greater than 1
+        if (strlength(ch) > 1 || (strlength(ch) == 1 && ch[0] != '\n')) {
+            // Clear screen command
+            if (strEql(ch, "clear")) {
+                clearScreen();
+            } 
+            // Display system info command
+            else if (strEql(ch, "info")) {
+                print("\n\nCOS 1.0 | Written by Coulter C. Stutz ");
+                print("\n   Bootloader written in 32 bit Assembly Language ");
+                print("\n       Compiled with NASM ");
+                print("\n   Kernel written in C ");
+                print("\n       Compiled with GCC ");
+                print("\n\nSome Include Libs forked from IKnowYourBrain ");
+                print("\n   His Bitcoin Address:1KEJNtrJAU3VEYyiBtELKra1Tq3XM5Ligk ");
+                print("\n\n\n   Buy Me Virtual Pizza! ");
+                print("\n   Bitcoin: 13jStC1PWwZttLc8rhG6Xn1UeKE7rAP6hQ ");
+                print("\n   ETH/BSC/MATIC: 0xa693190103733280E23055BE70C838d9b6708b9a ");
+                print("\n   Monero: 49zw4GkS6MAWffKoskZxba3WXRtLJPUpwbN5ufmHL2AgKzB7YjSWBoVPivtPmn1DVxLiSwdZc7QnkcFnvXUuWdK3RV1xhZZ ");
+            } 
+            // Command not recognized
+            else {
+                print("\nUnknown Command: ");
+                print(ch);
+            }
+
+            // Print additional newline for spacing
+            print("\n\n");
+        } 
+        // If input is too short, just print a newline
+        else {
+            print("\n");
+        }
+    }
 }
