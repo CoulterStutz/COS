8ac5f2327a0f9e5dffb9f041aff4ff46ebfa0fb6
Coulter <coulterstutz@gmail.com>
2024-01-09 18:18:05 -0700

    Screen: Move Funcs to C

--- src/include/screen.h
+++ src/include/screen.h
@@ -7,209 +7,37 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-#include "types.h"
-#include "sys.h"
-#include "string.h"
+#include "types.h"  // Assuming types.h defines uint8_t, uint16_t, etc.
 
-// Screen size and depth constants
-const uint8 sw = 80, sh = 25, sd = 2;
+// Screen size constants
+#define MAX_ROWS 25
+#define MAX_COLS 80
 
-// Cursor position
-int cursorX = 0, cursorY = 0;
+// Attribute byte for our default color scheme
+#define WHITE_ON_BLACK 0x0F
 
-// Clears a line range on the screen
-void clearLine(uint8 from, uint8 to) {
-    uint16 i = sw * from * sd;
-    string vidmem = (string)0xb8000;
-    for(; i < (sw * to * sd); i += 2) {
-        vidmem[i] = ' ';           // Set character to space
-        vidmem[i + 1] = 0x07;      // Set attribute byte (e.g., light grey on black)
-    }
-}
+// Screen device I/O ports
+#define REG_SCREEN_CTRL 0x3D4
+#define REG_SCREEN_DATA 0x3D5
 
+// Public kernel API
+void clearScreen();
+void print(char* message);
+void print_at(char* message, int col, int row);
+void print_char(char character, int col, int row, char attribute_byte);
+void set_cursor(int col, int row);
 
-// Updates the position of the cursor
-void updateCursor() {
-    unsigned temp = cursorY * sw + cursorX;
-    outportb(0x3D4, 14);
-    outportb(0x3D5, temp >> 8);
-    outportb(0x3D4, 15);
-    outportb(0x3D5, temp);
-}
+// You might also have functions for color management
+void print_red(char* message);
+void print_blue(char* message);
+void print_pink(char* message);
+void print_cyan(char* message);
+void print_green(char* message);
+void print_gold(char* message);
+void print_light_blue(char* message);
+void print_orange(char* message);
+void print_purple(char* message);
 
-// Clears the entire screen and resets cursor position
-void clearScreen() {
-    clearLine(0, sh+1);
-    cursorX = 0;
-    cursorY = 0;
-    updateCursor();
-}
+// ... other color functions ...
 
-// Scrolls the screen up by a given number of lines
-void scrollUp(uint8 lineNumber) {
-    string vidmem = (string)0xb8000;
-    uint16 i = 0;
-    clearLine(0, lineNumber - 1);
-    for(; i < sw * (sh - 1) * 2; i++) {
-        vidmem[i] = vidmem[i + sw * 2 * lineNumber];
-    }
-    clearLine(sh - 1 - lineNumber, sh - 1);
-    cursorY = (cursorY - lineNumber) < 0 ? 0 : cursorY - lineNumber;
-    cursorX = cursorY == 0 ? 0 : cursorX;
-    updateCursor();
-}
-
-// Checks if a new line is needed
-void newLineCheck() {
-    if(cursorY >= sh - 1) {
-        scrollUp(1);
-    }
-}
-
-// Prints a character on the screen at the cursor's position
-void printch(char c) {
-    string vidmem = (string)0xb8000;
-    switch(c) {
-        case (0x08): // Backspace
-            if(cursorX > 0) {
-                cursorX--;
-                vidmem[(cursorY * sw + cursorX) * sd] = 0x00;
-            }
-            break;
-        case ('\r'): // Carriage Return
-            cursorX = 0;
-            break;
-        case ('\n'): // New Line
-            cursorX = 0;
-            cursorY++;
-            break;
-        default: // Regular character
-            vidmem[((cursorY * sw + cursorX)) * sd] = c;
-            vidmem[((cursorY * sw + cursorX)) * sd + 1] = 0x0F; // Default color (white)
-            cursorX++;
-            break;
-    }
-    if(cursorX >= sw) {
-        cursorX = 0;
-        cursorY++;
-    }
-    updateCursor();
-    newLineCheck();
-}
-
-void printch_color(char c, uint8 color) {
-    string vidmem = (string) 0xb8000;
-    uint16 offset = (cursorY * sw + cursorX) * sd;
-
-    switch(c) {
-        case (0x08): // Backspace
-            if(cursorX > 0) {
-                cursorX--;
-                vidmem[offset - 2] = ' ';
-                vidmem[offset - 1] = color;
-            }
-            break;
-        case ('\r'): // Carriage Return
-            cursorX = 0;
-            break;
-        case ('\n'): // New Line
-            cursorX = 0;
-            cursorY++;
-            break;
-        default:
-            vidmem[offset] = c;
-            vidmem[offset + 1] = color;
-            cursorX++; 
-            break;
-    }
-    updateCursor(); // Update cursor position
-    newLineCheck(); // Check for new line
-}
-
-// Prints a string in default color
-void print(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for(; i < length; i++) {
-        printch(ch[i]);
-    }
-}
-
-void print_red(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for(; i < length; i++) {
-        printch_color(ch[i], 0x04); // Red color
-    }
-}
-
-void print_blue(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for(; i < length; i++) {
-        printch_color(ch[i], 0x01); // Blue color
-    }
-}
-
-void print_pink(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for(; i < length; i++) {
-        printch_color(ch[i], 0x05); // Pink (Light Magenta) color
-    }
-}
-
-void print_cyan(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for(; i < length; i++) {
-        printch_color(ch[i], 0x03); // Cyan color
-    }
-}
-
-// Function to print string in green
-void print_green(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for (; i < length; i++) {
-        printch_color(ch[i], 0x02); // Green color
-    }
-}
-
-// Function to print string in gold
-void print_gold(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for (; i < length; i++) {
-        printch_color(ch[i], 0x06); // Gold color
-    }
-}
-
-// Function to print string in light blue
-void print_light_blue(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for (; i < length; i++) {
-        printch_color(ch[i], 0x09); // Light Blue color
-    }
-}
-
-// Function to print string in orange
-void print_orange(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for (; i < length; i++) {
-        printch_color(ch[i], 0x0C); // Orange color
-    }
-}
-
-void print_purple(string ch) {
-    uint16 i = 0;
-    uint8 length = strlength(ch);
-    for (; i < length; i++) {
-        printch_color(ch[i], 0x05); // Purple color
-    }
-}
-
-
-#endif
+#endif // SCREEN_H
